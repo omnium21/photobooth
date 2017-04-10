9441fdca2c9281e21fc5625e352c47fa8aebad77
Ryan Harkin <ryan.harkin@linaro.org>
2017-04-10 16:00:19 +0100

    Pass DATA_DIR to showjpg instead of LOGS_DIR
    I want to expand the functionality in showjpg to use more directories
    inside the DATA_DIR, so instead of passing LOGS_DIR, pass DATA_DIR and
    let the script work out where the logs go.
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

--- photobooth.sh
+++ photobooth.sh
@@ -38,7 +38,7 @@ exec > file 2>&1
 ## Show a default 'boot splash image'
 ##
 
-$PB_DIR/showjpg $PB_DIR/photobooth.jpg $LOGS_DIR
+$PB_DIR/showjpg $PB_DIR/photobooth.jpg $DATA_DIR
 
 
 ##
@@ -49,7 +49,7 @@ $PB_DIR/showjpg $PB_DIR/photobooth.jpg $LOGS_DIR
 ##
 
 iwatch -e moved_to \
-	-c "$PB_DIR/showjpg %f $LOGS_DIR" \
+	-c "$PB_DIR/showjpg %f $DATA_DIR" \
 	$PHOTOS_DIR 2>&1 >> $LOGS_DIR/iwatch.log &
 
 ##
